8be4ea519102cfe0183b664cfbb49603561f2e2a
su-tran75 <su-tran@hotmail.fr>
2021-11-15 18:50:55 +0800

    create SVG circle progress

--- progress bar/app.js
+++ progress bar/app.js
@@ -1,26 +1,17 @@
-const progress = document.querySelector(".progress");
-const loading = document.querySelector(".loading");
-
-let i = 0;
-const fakeProgress = [0, 10, 35, 40, 42, 60, 70, 75, 90, 100];
-
-const interval = setInterval(() => {
-  progress.style.width = fakeProgress[i] + "%";
-  loading.innerHTML = fakeProgress[i] + "%";
-  i++;
-  if (i == fakeProgress.length) {
-    clearInterval(interval);
-  loading.innerHTML = "Completed";
-
-  }
-}, 1000);
+// const progress = document.querySelector(".progress");
+// const loading = document.querySelector(".loading");
 
 // let i = 0;
+// const fakeProgress = [0, 10, 35, 40, 42, 60, 70, 75, 90, 100];
 
 // const interval = setInterval(() => {
+//   progress.style.width = fakeProgress[i] + "%";
+//   loading.innerHTML = fakeProgress[i] + "%";
 //   i++;
-//   console.log("hello");
-//   if (i == 5) {
+//   if (i == fakeProgress.length) {
 //     clearInterval(interval);
+//   loading.innerHTML = "Completed";
+
 //   }
 // }, 1000);
+
