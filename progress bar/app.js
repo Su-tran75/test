1570f232285b295da22687af33b596692162e3cc
su-tran75 <su-tran@hotmail.fr>
2021-11-15 19:32:54 +0800

    create a function to fake progress with circle

--- progress bar/app.js
+++ progress bar/app.js
@@ -1,3 +1,22 @@
+const circle = document.querySelector(".progress-circle");
+const loading = document.querySelector(".loading");
+
+let i = 0;
+const fakeProgress = [0, 10, 42, 90, 100];
+
+const circumference = circle.getTotalLength();
+
+const interval = setInterval(() => {
+  circle.style.strokeDashoffset =
+    circumference - (fakeProgress[i] / 100) * circumference;
+  loading.innerHTML = fakeProgress[i] + "%";
+  i++;
+  if (i == fakeProgress.length) {
+    clearInterval(interval);
+    loading.innerHTML = "Done";
+  }
+}, 1000);
+
 // const progress = document.querySelector(".progress");
 // const loading = document.querySelector(".loading");
 
@@ -14,4 +33,3 @@
 
 //   }
 // }, 1000);
-
